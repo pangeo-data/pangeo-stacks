36f75bd3ffa488042e7d34df6b7dd27e69dbc5d6
Joseph Hamman <jhamman@ucar.edu>
2019-02-10 14:16:00 -0800

    use global env vars to create image names

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
+set -x
+
+echo "printing environment in push script"
+env
+echo "end printing of environment in push script"
+
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
+IMAGE_NAME=${DOCKER_ID}/${IMAGE}:${CALVER}
+IMAGE_LATEST=${DOCKER_ID}/${IMAGE}:latest
 
 echo "docker tag ${IMAGE_NAME} ${IMAGE_LATEST}"
 docker tag ${IMAGE_NAME} ${IMAGE_LATEST}
