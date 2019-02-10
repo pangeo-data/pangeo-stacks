962be354e65bcf3c7164f0dc30b11c33ac61a238
Joseph Hamman <jhamman@ucar.edu>
2019-02-10 14:29:40 -0800

    cleanup push script

--- docker_push.sh
+++ docker_push.sh
@@ -2,16 +2,11 @@
 
 set -x
 
-echo "printing environment in push script"
-env
-echo "end printing of environment in push script"
-
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
 IMAGE_NAME=${DOCKER_ID}/${IMAGE}:${CALVER}
 IMAGE_LATEST=${DOCKER_ID}/${IMAGE}:latest
 
-echo "docker tag ${IMAGE_NAME} ${IMAGE_LATEST}"
 docker tag ${IMAGE_NAME} ${IMAGE_LATEST}
 
 docker push ${IMAGE_NAME}
