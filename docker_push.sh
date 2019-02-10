992d687cf477ab10431a9319b1e7ac5ecce92aa6
Joseph Hamman <jhamman@ucar.edu>
2019-02-10 13:46:56 -0800

    debug push

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
+echo "docker tag ${IMAGE_NAME} ${IMAGE_LATEST}"
 docker tag ${IMAGE_NAME} ${IMAGE_LATEST}
 
-docker push $IMAGE_NAME
-docker push $IMAGE_LATEST
+docker push ${IMAGE_NAME}
+docker push ${IMAGE_LATEST}
