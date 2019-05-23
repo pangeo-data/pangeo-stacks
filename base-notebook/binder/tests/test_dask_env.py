85ae8f7a13ec077ddd164bb0456b10a7de4889eb
Scott Henderson <scottyhq@gmail.com>
2019-05-22 17:32:54 -0700

    added a couple dask config tests

--- base-notebook/binder/tests/test_dask_env.py
+++ base-notebook/binder/tests/test_dask_env.py
@@ -10,3 +10,11 @@ def client():
 def test_check_dask_version(client):
     print(client)
     versions = client.get_versions(check=True)
+
+
+def test_dask_config():
+    import dask
+
+    assert '/srv/conda/envs/notebook/etc/dask' in dask.config.paths
+    assert dask.config.config['labextension']['factory']['class'] == 'KubeCluster'
+    assert 'worker-template' in dask.config.config['kubernetes']
